6a9141132c4f2ef92d2bb73f2d82244a062f0838
lswang <lswang@wysource.com.cn>
2019-02-20 19:55:49 +0800

    代码优化

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-const types = require('@babel/types')
-
-// 将import {flattenDeep, chunk} from 'lodash' 转化为下面这种写法:
-// import flattenDeep from 'lodash/flattenDepp'
-// import chunk from 'lodash/chunk'
-
-// Babel将源码转换AST之后，通过遍历AST树（其实就是一个js对象），对树做一些修改，然后再将AST转成code，即成源码。
-let visitor = {
-  ImportDeclaration(path, ref = { opts: {} }) {
-    let node = path.node
-    let { specifiers } = node
-    if ('k12lib' === node.source.value && !types.isImportDefaultSpecifier(specifiers[0])) {
-      let newImports = specifiers.map(specifier => {
-        return types.importDeclaration(
-          [types.importDefaultSpecifier(specifier.local)],
-          types.stringLiteral(`${node.source.value}/components/${specifier.local.name}`)
-        )
-      })
-      path.replaceWithMultiple(newImports)
+module.exports = ( { types } ) => {
+  return {
+    visitor: {
+      ImportDeclaration(path) {
+        let node = path.node
+        let { specifiers } = node
+        if ('k12lib' === node.source.value && !types.isImportDefaultSpecifier(specifiers[0])) {
+          let newImports = specifiers.map(specifier => {
+            return types.importDeclaration(
+              [types.importDefaultSpecifier(specifier.local)],
+              types.stringLiteral(`${node.source.value}/components/${specifier.local.name}`)
+            )
+          })
+          path.replaceWithMultiple(newImports)
+        }
+      }
     }
   }
 }
-
-module.exports = function () {
-  return { visitor }
-};
